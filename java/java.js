a51fab20ab47c31712844dd2b4ca276270d3eb92
epasseno <44741478+epasseno@users.noreply.github.com>
2018-11-07 23:01:41 -0500

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,5 @@
+//links for the navigation//
+docuument.write("<a href='index.html'>Section 1</a>);
+docuument.write("<a href='page1.html'>Section 1</a>);
+docuument.write("<a href='page2.html'>Section 1</a>);
+docuument.write("<a href='sources.html'>Section 1</a>);
